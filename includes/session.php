494815ab3a39b0a15a5b5f7fd94db3488d534144
Mahdi Bazyar <gf9295@wayne.edu>
2019-10-22 12:30:14 -0400

    new php file added

---
+++ includes/session.php
@@ -0,0 +1,15 @@
+<?php session_start();
+if(!isset(  $_SESSION["loggedIn"]))
+{
+	$_SESSION["loggedIn"]  = false;
+}
+if($_SESSION["loggedIn"] == true)
+{
+	//echo "You are logged in";
+}
+else{
+	//echo "You are not logged in";
+}
+
+
+?>
