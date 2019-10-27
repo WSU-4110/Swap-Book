a816be829f4217ee9366bbaf81eb3b6c76ca2644
Mahfuj Rahman <fq6510@wayne.edu>
2019-10-27 14:06:19 -0400

    Java script added for User Review

---
+++ scripts/newRating.js
@@ -0,0 +1,46 @@
+﻿
+
+$("#NewRatingLevel1").click(function() {
+    handleRatingInput(1);
+});
+
+$("#NewRatingLevel2").click(function() {
+    handleRatingInput(2);
+});
+
+$("#NewRatingLevel3").click(function() {
+    handleRatingInput(3);
+});
+
+$("#NewRatingLevel4").click(function() {
+    handleRatingInput(4);
+});
+
+$("#NewRatingLevel5").click(function() {
+    handleRatingInput(5);
+});
+
+function handleRatingInput(ratingLevel) {
+    $("#hiddenRatingValue").val(ratingLevel);
+    $("#NewRatingLevel1").removeClass("StarChecked");
+    $("#NewRatingLevel2").removeClass("StarChecked");
+    $("#NewRatingLevel3").removeClass("StarChecked");
+    $("#NewRatingLevel4").removeClass("StarChecked");
+    $("#NewRatingLevel5").removeClass("StarChecked");
+    $("#NewRatingLevel1").addClass("StarChecked");
+
+
+    if (ratingLevel > 1) {
+        $("#NewRatingLevel2").addClass("StarChecked");
+    }
+    if (ratingLevel > 2) {
+        $("#NewRatingLevel3").addClass("StarChecked");
+    }
+    if (ratingLevel > 3) {
+        $("#NewRatingLevel4").addClass("StarChecked");
+    }
+    if (ratingLevel === 5) {
+        $("#NewRatingLevel5").addClass("StarChecked");
+    }
+
+}
