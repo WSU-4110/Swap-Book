ff2fd6654f5306a903314232978e6de3218725e4
Mahfuj Rahman <fq6510@wayne.edu>
2019-11-27 20:14:46 -0500

    Updated main connection file for Unit Testing

--- includes/mainConnection.php
+++ includes/mainConnection.php
@@ -1,16 +1,68 @@
-
 <?php
-/*$servername = "localhost";
-$username = "root";
-$password = "2020";
-$dbname = "swapbook";
+class mainConnection {
+    const BR = '<br />';
+    var $username;
+    var $password;
+    var $login;
+    
+    
+    function getUserName()
+    {
+        return $this->username;
+    }
+    
+	function setUserName($userN)
+    {
+        $this->username = $userN;
+    }
 
-// Create connection
-$mainConnection = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($mainConnection->connect_error) {
-    die("Connection failed: " . $mainConnection->connect_error);
+	
+    function getPassword()
+    {
+        return $this->password;
+    }
+    
+    function setPassword($passWord)
+    {
+        $this->password = $passWord;
+    }
+
+   
+
+    function getLogIn()
+    {
+        return $this->login;
+    }
+    
+    function setLogIn($Login)
+    {
+        $this->login = $Login;
+    }
+    
+
+	
+    public function display_connection(){
+           
+           echo $this->getUserName().self::BR;
+           echo $this->username.self::BR;
+           
+		   echo $this->getPassword().self::BR;
+           echo $this->password.self::BR;
+           
+		   echo $this->getLogIn().self::BR;
+           echo $this->login.self::BR;
+     }
 }
-echo "<!-- Connected successfully -->"; */
+
+
+$mainConnection = new mainConnection;
+
+$mainConnection-> setUserName('root');
+$mainConnection-> setPassword(2020);
+$mainConnection-> setLogIn('successful');
+
+$mainConnection->display_connection();
+
+
 ?>
